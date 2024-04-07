c1535467b112551f32a856ac1b07983e97e24ab6
uzzal71 <uzzalroy.acm@gmail.com>
2024-04-07 15:48:07 +0600

    node docker

--- node-docker/index.js
+++ node-docker/index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello World!</h1>");
+  res.send("<h1>Welcome nodejs application in docker.</h1>");
 });
 
 app.listen(port, (req, res) => {
