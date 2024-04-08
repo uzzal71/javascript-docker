38a340381123c98796caed74b77b4ac94d8cf5c1
uzzal71 <uzzalroy.acm@gmail.com>
2024-04-08 13:18:44 +0600

    node-docker

--- node-docker/index.js
+++ node-docker/index.js
@@ -1,7 +1,13 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+mongoose
+  .connect("mongodb://uzzal71:uzzal@123@172.26.0.3:27017/?authSource=admin")
+  .then(() => console.log("successfully connected to DB"))
+  .catch((e) => console.log(e));
 
 app.get("/", (req, res) => {
   res.send({
