e78a403f28cedd1f41494bb410ef4ba9422b36ab
uzzal71 <uzzalroy.acm@gmail.com>
2024-04-10 20:06:39 +0600

    node docker

--- node-dockerize/controllers/authController.js
+++ node-dockerize/controllers/authController.js
@@ -1,8 +1,16 @@
+import bcrypt from "bcryptjs";
 import User from "../models/userModel.js";
 
 export const signUp = async (req, res, next) => {
   try {
-    const newUser = await User.create(req.body);
+    const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const newUser = await User.create({
+      username,
+      password: hashedPassword,
+    });
+
     res.status(201).json({
       status: "success",
       data: { user: newUser },
@@ -10,6 +18,39 @@ export const signUp = async (req, res, next) => {
   } catch (error) {
     res.status(400).json({
       status: "fail",
+      error: error.message,
+    });
+  }
+};
+
+export const login = async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(404).json({
+        status: "fail",
+        message: "User not found",
+      });
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return res.status(401).json({
+        status: "fail",
+        message: "Invalid password",
+      });
+    }
+
+    res.status(200).json({
+      status: "success",
+      message: "Login successful",
+      data: { user },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      error: error.message,
     });
   }
 };
--- node-dockerize/routes/userRoutes.js
+++ node-dockerize/routes/userRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { signUp } from "../controllers/authController.js";
+import { login, signUp } from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.post("/signup", signUp);
+router.post("/login", login);
 
 export default router;
