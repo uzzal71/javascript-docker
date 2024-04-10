8fc3eb5a2e55f43e43840b032216d5917b5fa873
uzzal71 <uzzalroy.acm@gmail.com>
2024-04-10 14:52:33 +0600

    node docker

--- node-dockerize/index.js
+++ node-dockerize/index.js
@@ -1,23 +1,21 @@
 import bodyParser from "body-parser";
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 
+mongoose
+  .connect("mongodb://uzzal71:uzzal123@172.21.0.2:27017/?authSource=admin")
+  .then(() => console.log("successfully connected to DB"))
+  .catch((e) => console.log(e));
+
 app.get("/", (req, res) => {
   res.status(200).send({
     status: 200,
     message: "nodejs app is running..",
-    data: {
-      sliders: {
-        title: "Sliders",
-        description: "Sliders description",
-        image:
-          "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-        url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-      },
-    },
+    data: {},
   });
 });
 
