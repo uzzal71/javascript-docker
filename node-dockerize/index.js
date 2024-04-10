04b1acac1309e856de0f6e958a0e6c6602ef3a2b
uzzal71 <uzzalroy.acm@gmail.com>
2024-04-11 02:12:56 +0600

    node-doker

--- node-dockerize/index.js
+++ node-dockerize/index.js
@@ -25,7 +25,7 @@ const connectWithRetry = () => {
 
 connectWithRetry();
 
-app.get("/", (req, res) => {
+app.get("/api/v1", (req, res) => {
   res.status(200).send({
     status: 200,
     message: "nodejs app is running..",
