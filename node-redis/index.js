528eaf3fb2cc9b35067ce07f76b508dd8b3c4844
uzzal71 <uzzalroy.acm@gmail.com>
2024-04-11 19:23:14 +0600

    docker - node, redis

--- node-redis/index.js
+++ node-redis/index.js
@@ -6,6 +6,13 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.get("/", (req, res) => {
+  res.send({
+    status: "success",
+    message: "Welcome to node application",
+  });
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, (req, res) => {
   console.log("Server is running on port " + port);
