daa0db21baff4154314c9f954818ef19dd7f2d4d
uzzal71 <uzzalroy.acm@gmail.com>
2024-04-13 20:02:31 +0600

    docker

--- node-redis/db.js
+++ node-redis/db.js
@@ -1,3 +1,6 @@
 import mongoose from "mongoose";
 
-mongoose.connect(`mongodb:user01:123334@ip-address`).then();
+mongoose
+  .connect(`mongodb:user01:123334@ip-address`)
+  .then(() => console.log("Database connection established"))
+  .catch((err) => console.error(err));
